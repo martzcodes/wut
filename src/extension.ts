fee5c49be2afd09d80bdde7c5545fd7435994824
Matt Martz <matt.martz@gmail.com>
2016-05-22 11:09:01 -0400

    LCOV complete, displaying in active window

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,27 @@ import * as vscode from 'vscode';
 
 import fs = require('fs');
 import path = require('path');
+import * as _ from 'lodash';
+
+export interface ILineObject {
+    lineNumber: number;
+    functionName: string;
+    functionCovered: boolean;
+    lineCovered: boolean;
+    branchCovered: boolean;
+}
+
+export class LineObject implements ILineObject {
+    lineNumber: number;
+    functionName: string;
+    functionCovered: boolean = null;
+    lineCovered: boolean = null;
+    branchCovered: boolean = null;
+
+    constructor(lineNumber: number) {
+
+    }
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,7 +38,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.checkFile', () => {
         // The code you place here will be executed every time your command is executed
 
         let lcovPath = vscode.workspace.getConfiguration('wut').get('lcov') as string;
@@ -58,11 +79,27 @@ export function activate(context: vscode.ExtensionContext) {
                 overviewRulerColor: 'lightgreen'
             }
         });
+        let warning = vscode.window.createTextEditorDecorationType({
+            // isWholeLine: true,
+            // borderWidth: '2px',
+            // borderStyle: 'solid',
+            gutterIconPath: '/users/matthewmartz/Development/wut/images/warning.svg',
+            overviewRulerLane: vscode.OverviewRulerLane.Center,
+            light: {
+                // this color will be used in light color themes
+                overviewRulerColor: 'darkorange'
+            },
+            dark: {
+                // this color will be used in dark color themes
+                overviewRulerColor: 'lightorange'
+            }
+        });
 
-        let highlights: vscode.DocumentHighlight[] = [];
-        let notCoveredLines: vscode.DecorationOptions[] = [];
         let coveredLines: vscode.DecorationOptions[] = [];
-        vscode.window.showInformationMessage('Hello World!');
+        let warningLines: vscode.DecorationOptions[] = [];
+        let notCoveredLines: vscode.DecorationOptions[] = [];
+        let outChannel = vscode.window.createOutputChannel("wut");
+        // vscode.window.showInformationMessage('Hello World!');
 
         fs.readFile(path.join(vscode.workspace.rootPath, lcovPath), function (err, data) {
             if (err) {
@@ -74,51 +111,108 @@ export function activate(context: vscode.ExtensionContext) {
             matches.forEach((value) => {
                 let fileName = path.join(vscode.workspace.rootPath, value.match(/SF:([\s\S]*?)\n/)[1]);
                 if (fileName === activeEditor.document.fileName) {
-                    let lines = value.split('\n');
-                    let FNs = lines.filter(function(line){
+                    let rawLines = value.split('\n');
+                    let FNs = rawLines.filter(line => {
                         return line.indexOf('FN:') > -1;
                     });
-                    let FNDAs = lines.filter(function(line) {
+                    let DAs = rawLines.filter(line => {
+                        return line.indexOf('DA:') > -1 && line.indexOf('BRDA') === -1 && line.indexOf('FNDA') === -1;
+                    });
+                    let BRDAs = rawLines.filter(line => {
+                        return line.indexOf('BRDA:') > -1;
+                    });
+                    let FNDAs = rawLines.filter(line => {
                         return line.indexOf('FNDA:') > -1;
                     });
-                    let FNDA = FNDAs.map(function(line) {
+                    let FNDA = FNDAs.map(line => {
                         return line.split(',')[1] || '';
                     });
-                    let FN = FNs.map(function(line) {
-                        let temp = line.slice(3,line.length).split(',');
-                        return {
+                    let lines: LineObject[] = [];
+
+                    FNs.forEach(line => {
+                        let temp = line.slice(3, line.length).split(',');
+                        let rObj: LineObject = {
                             lineNumber: Number(temp[0]),
                             functionName: temp[1] || '',
-                            covered: FNDA.indexOf(temp[1] || '') !== -1
+                            functionCovered: FNDA.indexOf(temp[1] || '') !== -1,
+                            lineCovered: null,
+                            branchCovered: null
                         };
+                        lines.push(rObj);
                     });
-                    console.log(FN);
-                    console.log("file name matched");
+                    DAs.forEach(line => {
+                        let temp = line.slice(3, line.length).split(',');
+                        let lineIndex = _.findIndex(lines, { lineNumber: Number(temp[0]) });
+                        if (lineIndex > -1) {
+                            lines[lineIndex].lineCovered = Number(temp[1]) > 0;
+                        } else {
+                            let rObj: LineObject = {
+                                lineNumber: Number(temp[0]),
+                                functionName: '',
+                                functionCovered: null,
+                                lineCovered: Number(temp[1]) > 0,
+                                branchCovered: null
+                            };
+                            lines.push(rObj);
+                        }
+                    });
+                    BRDAs.forEach(line => {
+                        let temp = line.slice(5, line.length).split(',');
+                        let lineIndex = _.findIndex(lines, { lineNumber: Number(temp[0]) });
+                        if (lineIndex > -1) {
+                            lines[lineIndex].branchCovered = lines[lineIndex].branchCovered === false ? false : Number(temp[3]) > 0;
+                        } else {
+                            let rObj: LineObject = {
+                                lineNumber: Number(temp[0]),
+                                functionName: '',
+                                functionCovered: null,
+                                lineCovered: null,
+                                branchCovered: Number(temp[3]) > 0
+                            };
+                            lines.push(rObj);
+                        }
+                    });
+                    lines.sort((a,b) => {
+                        return a.lineNumber - b.lineNumber;
+                    });
+                    lines.forEach(line => {
+                        let lineRange = new vscode.Range(line.lineNumber - 1, 0, line.lineNumber - 1, activeEditor.document.lineAt(line.lineNumber).range.end.character);
+                        let coverCheck = (line.functionCovered === true || line.functionCovered === null) && (line.lineCovered === null || line.lineCovered === true) && (line.branchCovered === null || line.branchCovered === true);
+                        let notCoveredCheck = line.functionCovered === false || line.lineCovered === false || line.branchCovered === false;
+                        if (coverCheck) {
+                            let coveredDecoration = { range: lineRange, hoverMessage: '' };
+                            coveredLines.push(coveredDecoration);
+                        } else {
+                            let hoverMessage: string = 'Line ' + line.lineNumber + ' is NOT Covered by:';
+                            let sep = '';
+                            if (line.functionCovered === false) {
+                                hoverMessage += ' function';
+                                sep = ',';
+                            }
+                            if (line.lineCovered === false) {
+                                hoverMessage += sep + ' line';
+                                sep = ',';
+                            }
+                            if (line.branchCovered === false) {
+                                hoverMessage += sep + ' branch';
+                            }
+                            if (line.functionCovered === false && line.lineCovered === false && line.branchCovered === false) {
+                                let notCoveredDecoration = { range: lineRange, hoverMessage: hoverMessage };
+                                notCoveredLines.push(notCoveredDecoration);
+                            } else {
+                                let warningDecoration = { range: lineRange, hoverMessage: hoverMessage };
+                                warningLines.push(warningDecoration);
+                            }
+                            outChannel.show(true);
+                            outChannel.appendLine(hoverMessage);
+                        }
+                    });
+
+                    activeEditor.setDecorations(covered, coveredLines);
+                    activeEditor.setDecorations(warning, warningLines);
+                    activeEditor.setDecorations(notCovered, notCoveredLines);
                 }
             });
-            
-            // for (let lineNum: number = 0; lineNum < activeEditor.document.lineCount; lineNum++) {
-            //     let lineRange = new vscode.Range(lineNum, 0, lineNum, 0);
-            //     if (lineNum % 2 === 0) {
-            //         // even
-            //         let notCoveredDecoration = { range: lineRange, hoverMessage: '' };
-            //         notCoveredLines.push(notCoveredDecoration);
-            //     } else {
-            //         //odd
-            //         let coveredDecoration = { range: lineRange, hoverMessage: '' };
-            //         coveredLines.push(coveredDecoration);
-            //     }
-            // }
-
-            // let highlight = new vscode.DocumentHighlight(lineRange, vscode.DocumentHighlightKind.Text);
-            // highlights.push(highlight);
-
-            activeEditor.setDecorations(notCovered, notCoveredLines);
-            activeEditor.setDecorations(covered, coveredLines);
-
-            let outChannel = vscode.window.createOutputChannel("test");
-            outChannel.show(true);
-            outChannel.append("hello");
         });
     });
 
